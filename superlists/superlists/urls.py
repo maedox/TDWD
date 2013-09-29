13be3f401cb69b5ac5dc27eadd19605b0a1241a8
Pål Nilsen <paal.nilsen@gmail.com>
2013-09-29 18:40:55 +0200

    Changes action for forms, /lists now handles lists

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -1,13 +1,15 @@
-from django.conf.urls import patterns, include, url
+#from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
+urlpatterns = patterns(
+    '',
     url(r"^$", "lists.views.home_page", name="home"),
-    url(r"^lists/some-list-id/$", "lists.views.view_list", name="view_list")
+    url(r"^lists/some-list-id/$", "lists.views.view_list", name="view_list"),
+    url(r"^lists/new$", "lists.views.new_list", name="new_list"),
     # url(r'^superlists/', include('superlists.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
